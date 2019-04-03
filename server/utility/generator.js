0999db779f151a9196e1037b8bfa9455ff358cfa
Chris Varughese <cjamiev@icloud.com>
2019-04-02 22:17:18 -0500

    Adding in conversion of generator folder structure into json format - Will include files and the variables associated with them

--- server/utility/generator.js
+++ server/utility/generator.js
@@ -5,7 +5,18 @@ const GENERATOR_DIR = '../generator';
 const GENERATOR_FILE_EXT = '.hbs';
 const HANDLE_BARS_REGEX = /{{.+}}/;
 
-const getGeneratorsAsJSON = () => {
+const uniqueElementsOnly = (arr) => {
+  return arr.filter((item, pos) => arr.indexOf(item) === pos);
+}
+
+const getTemplateVariables = (template) => {
+  const reduceToVariablesOnly = template.split('}}').join('\n').match(/{{[a-zA-Z ]+/g).map(item => item.replace('{{','').trim());
+  const uniqueVariables = uniqueElementsOnly(reduceToVariablesOnly);
+
+  return uniqueVariables;
+}
+
+const getGenerators = () => {
   const allFiles = getAllFilesInTreeDirectory(GENERATOR_DIR);
   const foldersAndFiles = allFiles.map(item => item.split('\\').filter(item => item!=='..'&&item!=='generator'));
   const categorizedFoldersAndFiles = foldersAndFiles.map(item => {
@@ -16,39 +27,63 @@ const getGeneratorsAsJSON = () => {
       category, group, file
     }
   });
+
+  return categorizedFoldersAndFiles;
+}
+
+const getGeneratorsAsJSON = () => {
+  const categorizedFoldersAndFiles = getGenerators();
+
   const foldersAndFilesJSON = categorizedFoldersAndFiles.reduce((total, item)=>{
     if(total[item.category] && total[item.category][item.group]){
-      const mergedGroup = total[item.category][item.group].concat(item.file);
-      const mergedCategory = { [item.category]: {[item.group]: mergedGroup}};
+      const template = fs.readFileSync(`..\\generator\\${item.category}\\${item.group}\\${item.file}`, 'utf8');
+      const templateVariables = getTemplateVariables(template);
+      const reducedTemplateVariables = uniqueElementsOnly(templateVariables.concat(total[item.category][item.group].variables));
+      const mergedGroup = total[item.category][item.group].file.concat(item.file);
+      const mergedCategory = { [item.category]: {[item.group]: {file: mergedGroup, variables: reducedTemplateVariables}}};
       return {...total, ...mergedCategory};      
     }
     if(total[item.category]){
-      const mergedCategory = { [item.category]: {[item.group]: item.file, ...total[item.category]}}
+      const template = fs.readFileSync(`..\\generator\\${item.category}\\${item.group}\\${item.file}`, 'utf8');
+      const templateVariables = getTemplateVariables(template);
+      const mergedCategory = { [item.category]: {[item.group]: {file: item.file, variables: templateVariables}, ...total[item.category]}};
       return {...total, ...mergedCategory};      
     }
-    return {...total, [item.category]: {[item.group]: item.file}};
+    const template = fs.readFileSync(`..\\generator\\${item.category}\\${item.group}\\${item.file}`, 'utf8');
+    const templateVariables = getTemplateVariables(template);
+    return {...total, [item.category]: {[item.group]: {file: item.file, variables: templateVariables} }};
   }, {});
 
   return foldersAndFilesJSON;
 }
 
-const getCustomizedFile = (file,fileName) => {
-  const lines = file.split('\n');
-  const customizedLines = lines
-    .map(line => (HANDLE_BARS_REGEX.test(line)) ? line.replace(HANDLE_BARS_REGEX, fileName) : line);
+const replaceVariables = (line,variables) => {
+  const regexes = Object.keys(variables).map(item => `{{${item}}}`).join('|');  
+  const regex = RegExp(regexes,'g');
+
+  const newLine = line.replace(regex, (matched) => {
+    const newMatched = matched.replace(/{|}/g,'')
+    return mapObj[newMatched];
+  });
+
+  return newLine;
+}
+
+const getCustomizedFile = (file,variables) => {
+  const customizedLines = file.split('\n').map(line => (HANDLE_BARS_REGEX.test(line,variables)) ? replaceVariables(line): line);
 
   return customizedLines.join('\n');
 };
 
-const generateFile = (generatorType, fileType, fileName) => {
-  if(!generatorType||!fileType||!fileName){
-    console.log('Arguments Error: file name, file type and generator type is required');
+const generateFile = (generatorType, fileType, variables) => {
+  if(!generatorType||!fileType||!variables){
+    console.log('Arguments Error: variables, file type and generator type is required');
     return;
   }
 
   try {
     const template = fs.readFileSync(`${GENERATOR_DIR}/${generatorType}/${fileType}${GENERATOR_FILE_EXT}`, 'utf8');
-    const customizedFile = getCustomizedFile(template,fileName);
+    const customizedFile = getCustomizedFile(template,variables);
 
     fs.writeFileSync(`${fileName}.js`, customizedFile);
   } catch(err){
