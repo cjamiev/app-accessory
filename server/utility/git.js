4e306f23ee5a590deb09e845c357bfdf7fe777e3
Chris Varughese <cjamiev@icloud.com>
2019-04-02 22:18:28 -0500

    Adding in method for getting stale branches and script for deleting them

--- server/utility/git.js
+++ server/utility/git.js
@@ -1,5 +1,6 @@
 const exec = require('child_process').exec; 
 const execSync = require('child_process').execSync; 
+const DAYS_IN_SECONDS = 1000 * 60 * 60 * 24;
 
 const getListOfModifiedFiles = (file) => {
   const lines = file.split('\n');
@@ -8,6 +9,29 @@ const getListOfModifiedFiles = (file) => {
   
   return listOfModifiedFiles.join('\n');
 };
+
+const shouldIgnoreBranch = (branch, branchesToIgnore) => {
+  const matched = branchesToIgnore.filter(branchToIgnore => branch.includes(branchToIgnore));
+
+  return matched.length !== 0;
+}
+
+const isMoreThanSpecifiedDays = (givenDate,numberOfDays) => {
+  const now = (new Date()).getTime();
+  const compareDate = (new Date(givenDate)).getTime();
+  const diffInDays = (now - compareDate) / DAYS_IN_SECONDS;
+
+  return Number(diffInDays) > numberOfDays;
+}
+
+const createPruningScript = (branches, branchesToIgnore, numberOfDays) => {
+  const staleBranches = branches.filter(branch => !shouldIgnoreBranch(branch, branchesToIgnore) && isMoreThanSpecifiedDays(branch.slice(0,10),numberOfDays));
+  const staleBranchesFile = staleBranches.join('\n');
+  const pruneScript = staleBranches.map(line => `git push origin --delete ${line.slice(18)} \n`);
+  const pruneScriptFile = pruneScript.join('\n');
+
+  return { staleBranchesFile, pruneScriptFile };
+}
 
 const copyModifiedFiles = (projectDirectory, branchName) => {
   const cdIntoDirectory = `cd ${projectDirectory}`;
@@ -23,4 +47,15 @@ const copyModifiedFiles = (projectDirectory, branchName) => {
   return modifiedFiles;
 };
 
+const pruneStaleBranches = (branchName, branchesToIgnore, numberOfDays) => {
+  const gitRetrieveBranchesCommand = `git for-each-ref --merged ${branchName} --sort=committerdate refs/remotes/ --format="%(committerdate:short) %(refname:short)"`;
+
+  const result = execSync(gitRetrieveBranchesCommand, { encoding: 'utf8' });
+  const branchesSortedByDate = result.split('\n').sort();
+  const { staleBranchesFile, pruneScriptFile } = createPruningScript(branchesSortedByDate, branchesToIgnore, numberOfDays);
+
+  return { staleBranchesFile, pruneScriptFile };
+}
+
 module.exports.copyModifiedFiles = copyModifiedFiles;
+module.exports.pruneStaleBranches = pruneStaleBranches;
