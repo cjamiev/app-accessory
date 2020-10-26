4ad8a528fdb878060d84cb447d2ca747b5525399
Chris Varughese <cjamiev@icloud.com>
2020-10-25 21:21:37 -0500

    Adding more to snippets page but creating elements dynamically Updating clipboard for when adding lists. serve.bat will default to port 1000

--- README.md
+++ README.md
@@ -5,19 +5,33 @@ This project is designed to improve work efficiency. It features a UI with the f
 - Timer for events (pop up alert when done)
 - Links to saved pages
 
-## Quick Clipboard
+## Testing
+q.js at the base of the app is used for quick javascript coding/testing.  Run 'node q.js' to execute. Not meant for saving. 
+The root index.html is used for quick testing of html,css,javascript
+
+## Home
+### Quick Clipboard
 Quickly add copy buttons or timers.  Can be quickly deleted as well.
 
 ![Quick Clipboard Feature](./documentation/quick-clipboard-feature.png)
 
-## Copy blocks
-Displays content that would be copied to clipboard. 
-
-## Read/Write files
-Form for reading and writing to files.  Files must be deleted manually at the moment.  Shift+Enter (for adding new lines) must be used to avoid adding hidden divs to content.
+### Read/Write files
+Form for reading and writing to files.  Files must be deleted manually at the moment.  
 - Ascending/Descending Sort
 - Splitting into seperate Lines by delimiter
 - Copy content of file to clipboard
 - Validate JSON format
 
+**Note there is a wierd bug where sometimes formatting code gets into content depending how content was added in. Double check in notepadd++ when copy/pasting**
+
 ![Read and Write Feature](./documentation/read-write-feature.png)
+
+## Snippets
+This page is used to store commonly used html tags, javascript functions, css and other code snippets
+
+## Clipboard
+Page to store executable commands, lists, favorite links, etc. 
+
+## New
+Template page for adding in new pages. 
+
--- src/static/snippets/snippets.js
+++ src/static/snippets/snippets.js
@@ -1,37 +1,63 @@
-const dateMethods = [
-  { key: 'year', label: 'getFullYear()', comments: 'Get the year as a four digit number (yyyy)' },
-  { key: 'month', label: 'getMonth()', comments: 'Get the month as a number (0-11)' },
-  { key: 'date', label: 'getDate()', comments: 'Get the day as a number (1-31)' },
-  { key: 'hour', label: 'getHours()', comments: 'Get the hour (0-23)' },
-  { key: 'minute', label: 'getMinutes()', comments: 'Get the minute (0-59)' },
-  { key: 'second', label: 'getSeconds()', comments: 'Get the second (0-59)' },
-  { key: 'millisecond', label: 'getMilliseconds()', comments: 'Get the millisecond (0-999)' },
-  { key: 'day', label: 'getDay()', comments: 'Get the weekday as a number (0-6)' },
-  { key: 'time', label: 'getTime()', comments: 'Get the time (milliseconds since January 1, 1970)' }
-];
-
-const loadDateMethod = key => {
-  const dateMethod = dateMethods.find(item => item.key === key);
-  document.getElementById('copy-snippet').innerHTML = dateMethod.label;
-  document.getElementById('comments').innerHTML = dateMethod.comments;
-  copyHTMLContentToClipboard('copy-snippet');
-};
-
 const htmlTags = [
-  { key: 'html-page', label: '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="utf-8">\n\t<link rel="stylesheet" href="css/styles.css">\n\t<title>Your Website</title>\n</head>\n<body>\n\t<script src="js/scripts.js"></script>\n</body>\n</html>' },
-  { key: 'textual-tags', label: '<h1>This is a h1</h1>\n<p> This is a p</p>\n<label> This is a label </label>' },
-  { key: 'semantical-tags', label: '<div> This is a div </div>\n<span> This is a span </span>\n<header>This is a header</header>\n<nav>This is a nav</nav>\n<main> This is a main</main>\n<section>This is a section</section>\n<article>This is an article</article>\n<aside>This is an aside</aside>\n<footer>This is an footer</footer>\n' },
-  { key: 'input-tags', label: '<input type="button" value="A button"/>\n<input type="text"/>\n<input type="password"/>\n\n<input type="radio" name="radiogroup" value="Radio Button 1" />\n<input type="radio" name="radiogroup" value="Radio Button 2" />\n\n<input type="checkbox" value="Checkbox Button 1" />\n\n<select name="selectinput">\n\t<option value="value1">Selection 1</option>\n\t<option value="value2">Selection 2</option>\n</select>\n' },
-  { key: 'form', label: '<form>\n\t<fieldset>\n\t\t<legend>Personalia:</legend>\n\t\t<label for="name">Name:</label>\n\t\t<input id="name" type="text"><br>\n\t\t<label for="email">Email:</label>\n\t\t<input id="email" type="text"><br>\n\t\t<label for="dob">Date of birth:</label>\n\t\t<input id="dob" type="text"><br>\n\t\t<input type="submit" value="Submit">\n\t</fieldset>\n</form>\n' },
-  { key: 'table-tags', label: '<table>\n\t<thead>\n\t\t<tr>\n\t\t\t<th>Column 1 Header</th>\n\t\t</tr>\n\t</thead>\n\t<tbody>\n\t\t<tr>\n\t\t\t<td>Cell 11</td>\n\t\t</tr>\n\t\t<tr>\n\t\t\t<td>Cell 21</td>\n\t</tbody>\n</table>' },
-  { key: 'list-tags', label: '<ul>\n\t<li>List 1</li>\n\t<li>List 2</li>\n</ul>' },
-  { key: 'link-tags', label: '<a href="http://www.google.com" target="_blank">This is a hyperlink</a>\n<img src="smiley.gif" alt="Smiley face" height="42" width="42">' },
-  { key: 'seperation-tags', label: '<br/>\n<hr>' }
+  { label: 'Html Page', value: '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="utf-8">\n\t<link rel="stylesheet" href="css/styles.css">\n\t<title>Your Website</title>\n</head>\n<body>\n\t<script src="js/scripts.js"></script>\n</body>\n</html>' },
+  { label: 'Textual Tags', value: '<h1>This is a h1</h1>\n<p> This is a p</p>\n<value> This is a value </value>' },
+  { label: 'Semantical Tags', value: '<div> This is a div </div>\n<span> This is a span </span>\n<header>This is a header</header>\n<nav>This is a nav</nav>\n<main> This is a main</main>\n<section>This is a section</section>\n<article>This is an article</article>\n<aside>This is an aside</aside>\n<footer>This is an footer</footer>\n' },
+  { label: 'Input Tags', value: '<input type="button" value="A button"/>\n<input type="text"/>\n<input type="password"/>\n\n<input type="radio" name="radiogroup" value="Radio Button 1" />\n<input type="radio" name="radiogroup" value="Radio Button 2" />\n\n<input type="checkbox" value="Checkbox Button 1" />\n\n<select name="selectinput">\n\t<option value="value1">Selection 1</option>\n\t<option value="value2">Selection 2</option>\n</select>\n' },
+  { label: 'Form', value: '<form>\n\t<fieldset>\n\t\t<legend>Personalia:</legend>\n\t\t<value for="name">Name:</value>\n\t\t<input id="name" type="text"><br>\n\t\t<value for="email">Email:</value>\n\t\t<input id="email" type="text"><br>\n\t\t<value for="dob">Date of birth:</value>\n\t\t<input id="dob" type="text"><br>\n\t\t<input type="submit" value="Submit">\n\t</fieldset>\n</form>\n' },
+  { label: 'Table Tags', value: '<table>\n\t<thead>\n\t\t<tr>\n\t\t\t<th>Column 1 Header</th>\n\t\t</tr>\n\t</thead>\n\t<tbody>\n\t\t<tr>\n\t\t\t<td>Cell 11</td>\n\t\t</tr>\n\t\t<tr>\n\t\t\t<td>Cell 21</td>\n\t</tbody>\n</table>' },
+  { label: 'List Tags', value: '<ul>\n\t<li>List 1</li>\n\t<li>List 2</li>\n</ul>' },
+  { label: 'Link Tags', value: '<a href="http://www.google.com" target="_blank">This is a hyperlink</a>\n<img src="smiley.gif" alt="Smiley face" height="42" width="42">' },
+  { label: 'Seperation Tags', value: '<br/>\n<hr>' }
 ];
 
-const loadHTMLTags = key => {
-  const htmlTag = htmlTags.find(item => item.key === key);
-  document.getElementById('copy-snippet').innerHTML = replaceHTMLCharactersWithEscapeCharacters(htmlTag.label);
-  document.getElementById('comments').innerHTML = htmlTag.comments || '';
+const dateMethods = [
+  { label: 'Constructor', value: 'Date(year, monthIndex [, day [, hours [, minutes [, seconds [, milliseconds]]]]])' },
+  { label: 'Year', value: 'getFullYear()', comments: 'Get the year as a four digit number (yyyy)' },
+  { label: 'Month', value: 'getMonth()', comments: 'Get the month as a number (0-11)' },
+  { label: 'Date', value: 'getDate()', comments: 'Get the day as a number (1-31)' },
+  { label: 'Hour', value: 'getHours()', comments: 'Get the hour (0-23)' },
+  { label: 'Minute', value: 'getMinutes()', comments: 'Get the minute (0-59)' },
+  { label: 'Second', value: 'getSeconds()', comments: 'Get the second (0-59)' },
+  { label: 'Millisecond', value: 'getMilliseconds()', comments: 'Get the millisecond (0-999)' },
+  { label: 'Day', value: 'getDay()', comments: 'Get the weekday as a number (0-6)' },
+  { label: 'Time', value: 'getTime()', comments: 'Get the time (milliseconds since January 1, 1970)' }
+];
+
+const eventMethods = [
+  { label: 'Event Listener', value: 'addEventListener(event, function)' },
+  { label: 'Click', value: 'onclick' },
+  { label: 'Change', value: 'onchange' },
+  { label: 'Mouse Over', value: 'onmouseover' },
+  { label: 'Mouse Out', value: 'onmouseout' },
+  { label: 'Mouse Up', value: 'onmouseup' },
+  { label: 'Mouse Down', value: 'onmousedown' },
+  { label: 'Mouse Move', value: 'onmousemove' },
+  { label: 'Key Up', value: 'onkeyup' },
+  { label: 'Key Down', value: 'onkeydown' },
+  { label: 'Key Press', value: 'onkeypress' },
+  { label: 'Drag', value: 'ondrag' },
+  { label: 'Blur', value: 'onblur' },
+  { label: 'Focus', value: 'onfocus' },
+  { label: 'Load', value: 'onload' },
+  { label: 'Unload', value: 'onunload' }
+];
+
+const storageMethods = [
+  { label: 'Storage', value: 'localStorage', comments: 'Use sessionStorage if you do not want it to persist' },
+  { label: 'Add', value: 'setItem(k,v)' },
+  { label: 'Read', value: 'getItem(k)' },
+  { label: 'Delete', value: 'removeItem(k)' },
+  { label: 'Clear', value: 'clear()' }
+];
+
+const loadData = (label, data, escapeHTML = false) => {
+  const entry = data.find(item => item.label === label);
+  document.getElementById('copy-snippet').innerHTML = escapeHTML ? replaceHTMLCharactersWithEscapeCharacters(entry.value) : entry.value;
+  document.getElementById('comments').innerHTML = entry.comments || '';
   copyHTMLContentToClipboard('copy-snippet');
 };
+
+const loadHTMLTags = label => loadData(label, htmlTags, true);
+const loadEventMethod = label => loadData(label, eventMethods);
+const loadDateMethod = label => loadData(label, dateMethods);
+const loadStorageMethod = label => loadData(label, storageMethods);
