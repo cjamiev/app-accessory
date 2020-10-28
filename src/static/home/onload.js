037a798d46fe177ad9a86e9092fc9439fe9e7475
Chris Varughese <cjamiev@icloud.com>
2020-10-27 22:38:59 -0500

    Adding sanitization to all pages that need it.

--- src/static/home/onload.js
+++ src/static/home/onload.js
@@ -10,15 +10,3 @@ setInterval(() => {
 const today = new Date();
 const displayDate = dayOfWeek[today.getDay()] + ', ' + months[today.getMonth()] + ' ' + today.getDate() + ', ' + today.getFullYear();
 document.getElementById('current-date').innerHTML = displayDate;
-
-const editableElements = document.querySelectorAll('[contenteditable]');
-editableElements.forEach(element => {
-  element.addEventListener('paste', () => {
-    setTimeout(() => {
-      const currentContent = element.innerHTML;
-      const sanitizedContent = currentContent.replace(/(<([^>]+)>)/ig, '');
-
-      element.innerHTML = sanitizedContent;
-    }, 50);
-  });
-});
