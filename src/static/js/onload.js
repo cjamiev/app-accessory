db5030571b7e8d559476db1e810c7256316b1dc7
Chris Varughese <cjamiev@icloud.com>
2020-11-05 14:29:55 -0600

    Updating week function to be more precise and drop decimal.

--- src/static/js/onload.js
+++ src/static/js/onload.js
@@ -5,6 +5,9 @@ setInterval(() => {
 }, 1000);
 
 const today = new Date();
+const newYears = new Date(today.getFullYear(), 0, 1, 0, 0, 0);
+const firstWeekPartial = (newYears.getDay() / 7) + 1.01;
+
 const displayDate = dayOfWeek[today.getDay()] + ', ' + months[today.getMonth()] + ' ' + today.getDate();
 document.getElementById('current-date').innerHTML = displayDate;
-document.getElementById('current-week').innerHTML = 'Week ' + (1 + weeksBetween(new Date(2020, 0, 1, 0, 0, 0), new Date()));
+document.getElementById('current-week').innerHTML = 'Week ' + Math.trunc(firstWeekPartial + weeksBetween(newYears, today));
